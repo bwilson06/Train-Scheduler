e3567f4f275057bdba7c34fb181907208a03016c
Bradley Wilson <bluedirtbike6@gmail.com>
2020-05-02 11:33:18 -0500

    pushed up working changes

--- javascript/app.js
+++ javascript/app.js
@@ -4,6 +4,7 @@ var destination = ''
 var frequency;
 var firstTrain = ''
 
+
 var config = {
     apiKey: "AIzaSyC0DWE1pTO0iWUnd1yBb-92wuSfmb1FW9g",
     authDomain: "uw-bootcamp-bw.firebaseapp.com",
@@ -15,12 +16,50 @@ var config = {
     var database = firebase.database();
 
     $("#add-train").on("click", function(){
+        event.preventDefault()
         trainName = $("#name-input").val().trim()
         destination = $("#destination").val().trim()
         frequency = $("#frequency").val().trim()
-        firstTrain = $("#example-date-input").val().trim()
-        console.log(trainName)
+        firstTrain = $(".datetimepicker-input").val().trim()
+        
 
+        database.ref().push({
+            trainName: trainName,
+            destination: destination,
+            frequency: frequency,
+            firstTrainTime: firstTrain,
+          });
+          
     })
+
+    function update() {
+       var realTime = moment().format('HHmm');
+        database.ref().update({
+        realWorldTime : realTime
+        });
+    }
+
+    
+
+    
+
+    database.ref().on("child_added", function(childSnapshot) {
+
+        var firstTrainNew = moment(childSnapshot.val().firstTrainTime, "hh:mm").subtract(1, "years");
+        console.log(firstTrainNew)
+        var diffTime = moment().diff(moment(firstTrainNew), "minutes");
+        var remainder = diffTime % childSnapshot.val().frequency;
+        console.log(remainder)
+        var minAway = childSnapshot.val().frequency - remainder;
+        var nextTrain = moment().add(minAway, "minutes");
+        nextTrain = moment(nextTrain).format("hh:mm");
+        
+        $(".table").append('<tr><td>' + childSnapshot.val().trainName + '</td>' + '<td>' + childSnapshot.val().destination + '</td>' + '<td>' + childSnapshot.val().frequency + '</td>' + '<td>' + nextTrain + '</td>' +  '<td>' + minAway + '</td>' + '</tr>')
+
+    }, function(errorObject) {
+        console.log("Errors handled: " + errorObject.code)
+    });
+
+    setInterval(update, 1000)
    
    
